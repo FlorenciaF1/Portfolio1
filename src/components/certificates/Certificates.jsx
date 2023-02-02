4a06537e478a346d629df702bac6b0cac7222323
Florencia Fernandez <96849628+FlorenciaF1@users.noreply.github.com>
2023-02-02 15:00:01 -0300

    Portfolio

--- src/components/certificates/Certificates.jsx
+++ src/components/certificates/Certificates.jsx
@@ -1,16 +1,33 @@
 import React from 'react'
 import './certificates.css'
 
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
 const Explore = () => {
   return (
     <div className='certificates-container'>
-          <a>Certificates</a>
+          <button  class="accordion">Certificates</button >
+          <div class="panel">
             <a href='https://certificates.soyhenry.com/cert?id=b8170bdc-7e5d-485a-b3cd-6e2cd43e1ab7' target='_blank'>Full Stack Web Developer</a>
             <a href='https://www.efset.org/cert/qmNebq' target='_blank'>B2 Upper Intermediate</a>
             <a href='https://www.credly.com/badges/85c9081f-979b-49d7-9dce-28ce7cdb20d8/public_url' target='_blank'>Scrum Foundation Professional Certificate</a>
             <a href='https://drive.google.com/file/d/1pyf2nd2BiQ0w_ROqkBcu4HrV4ut3O1RI/view?usp=sharing' target='_blank'>Introduccion al Desarrollo Web I</a>
             <a href='https://drive.google.com/file/d/1kI8eUY_KUKXuVl_OXJFxHriUA52kerMs/view?usp=sharing' target='_blank'>Introduccion al Desarrollo Web II</a>
             <a href='https://www.educacionit.com/perfil/florencia-fernandez-861340/certificado/54677' target='_blank'>Introducción a la Programación</a>
+          </div>
     </div>
   )
 }
--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -2,44 +2,63 @@ import React from 'react'
 import './profile.css'
 
 
+var acc = document.getElementsByClassName("accordion1");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
 const Profile = () => {
   return (
     <div className='profile-container'>
 
-      <div>
-        <a className='a'>Recomendaciones</a>
+      <div className='title1'>
+        <a className="r0"></a>
+        <button class="accordion1">Recommendations</button >
       </div>
 
-      <div >
-        <a className="r1"></a>
-        <a className='a'>Juan Camilo Codina Ariza</a>
-      </div>
+      <div className="panel">
 
-      <div>
-        <a className="r2"></a>
-        <a className='a'>Paulina Gadea</a>
-      </div>
+        <div>
+          <a className="r1"></a>
+          <a className='a'> Juan Camilo Codina Ariza </a>
+        </div>
 
-      <div>
-        <a className="r3"></a>
-        <a className='a'>Walter Celiz</a>
-      </div>
+        <div>
+          <a className="r2"></a>
+          <a className='a'>Paulina Gadea</a>
+        </div>
 
-      <div>
-        <a className="r4"></a>
-        <a className='a'>Marlon moreno Pérez</a>
-      </div>
+        <div>
+          <a className="r3"></a>
+          <a className='a'>Walter Celiz</a>
+        </div>
 
-      <div>
-        <a className="r5"></a>
-        <a className='a'>Julio Andrés Cariajano Reyes</a>
-      </div>
+        <div>
+          <a className="r4"></a>
+          <a className='a'>Marlon moreno Pérez</a>
+        </div>
 
-      <div>
-        <a className="r6"></a>
-        <a className='a'>Ver más</a>
-      </div>
+        <div>
+          <a className="r5"></a>
+          <a className='a'>Julio Andrés Cariajano Reyes</a>
+        </div>
 
+        <div>
+          <a className="r6"></a>
+          <a className='a'>Ver más</a>
+        </div>
+
+      </div>
     </div>
   )
 }
